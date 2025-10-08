6714674df7bdea99d686ff628228812195c9535b
orion28k <160691449+orion28k@users.noreply.github.com>
2025-10-08 17:08:08 -0400

    Impliment tests

---
+++ DBConnector.Tests/PostgressConnector.Test.cs
@@ -0,0 +1,52 @@
+﻿using Testcontainers.MongoDb;
+
+namespace DBConnector.Tests;
+
+public class PostgresConnector : IAsyncLifetime
+{
+    private readonly PostgresDbContainer _postgresDbContainer;
+
+    public PostgresConnector()
+    {
+        _postgresDbContainer = new PostgresDbBuilder()
+            .WithImage("postgres:7.0")
+            .WithCleanUp(true)
+            .Build();
+    }
+
+    public async Task InitializeAsync()
+    {
+        await _postgresDbContainer.StartAsync();
+    }
+
+    public async Task DisposeAsync()
+    {
+        await _postgresDbContainer.DisposeAsync();
+    }
+
+    [Fact]
+    public async Task should_ping_db_successfully()
+    {
+        // Given
+        DataStore connector = new DBConnector.PostgresConnector(_postgresDbContainer.GetConnectionString());
+
+        // When
+        bool ping_result = await connector.ping();
+
+        // Then
+        Assert.True(ping_result);
+    }
+
+    [Fact]
+    public async Task should_fail_missing_db()
+    {
+        // Given
+        var connector = new DBConnector.PostgresConnector("");
+
+        // When
+        bool ping_result = await connector.ping();
+
+        // Then
+        Assert.False(ping_result);
+    }
+}
